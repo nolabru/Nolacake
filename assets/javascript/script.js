03bdb6fcbca36040c4518d770a9cae4f033f9623
Bruno Nolasco <139811133+nolabru@users.noreply.github.com>
2024-10-29 21:47:04 -0300

    Criação de Projeto Loja de Cupcakes

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+function passwordReveal(){
+    let senha = document.getElementById('password');
+    let verSenha = document.getElementById('eye');
+
+    if(senha.type === 'password'){
+        senha.setAttribute('type', 'text');
+        verSenha.classList.add('reveal');
+    }
+    else{
+        senha.setAttribute('type', 'password');
+        verSenha.classList.remove('reveal');
+    }
+
+}
